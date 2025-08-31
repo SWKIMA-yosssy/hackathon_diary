088480f6d0b71a1950c5a3113592ded0e89e28da
n4mlz <n4mlz.dev@gmail.com>
2025-08-31 20:47:46 +0900

    feat: ログインのサンプルコードを実装

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+import SignIn from "@/components/SignInButton";
+import SignOut from "@/components/SignoutButton";
+import { auth } from "@/lib/auth";
+
+export default async function Home() {
+  const session = await auth();
+
   return (
     <div>
       <h1>Hello world!</h1>
+      {session ? (
+        <div>
+          <div>{session.user?.name}</div>
+          <SignOut />
+        </div>
+      ) : (
+        <SignIn />
+      )}
     </div>
   );
 }
